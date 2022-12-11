f311478e9f4a39fcb2731ffd8d3ad7bee64bdd0c
Dian <emailforgong@gmail.com>
2022-12-11 22:14:19 +0800

    updated script

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@
 #fi
 
 # --- make a list of dependencies and then check all in one chunk of code
-# TODO
+# TODO DONE
 
 # --- to add checking of the result of each step, to make this robust
 # TODO
@@ -31,52 +31,44 @@
 # just asking
 
 PACKMANAGER=''
-PACK=''
-while true; do
-	printf "What package manager do you use ? \n1. apt \n2. pacman \n3. homebrew"
-	read -p "Type your choice [1/2/3]: " choice
-	case $choice in
-		1) PACKMANAGER="apt install"
-			break;;
-		2) PACKMANAGER="pacman -S"
-			break;;
-		3) PACKMANAGER="brew install"
-			break;;
-		0) exit
-			break;;
-		*) echo "invalid choice!"
-	esac
+PACKS="curl git zsh tmux"
+
+#while true; do
+#	printf "What package manager do you use ? \n1. apt \n2. pacman \n3. homebrew\n"
+#	read -p "Type your choice [1/2/3]: " choice
+#	case $choice in
+#		1) PACKMANAGER="apt install"
+#			break;;
+#		2) PACKMANAGER="pacman -S"
+#			break;;
+#		3) PACKMANAGER="brew install"
+#			break;;
+#		0) exit
+#			break;;
+#		*) echo "invalid choice!"
+#	esac
+#done
+
+denpendency_not_met=''
+echo ":: Checking dependencies..."
+for PACK in $PACKS;do
+echo ":: Checking if $PACK is installed ..."
+	if ! command -v $PACK 1> /dev/null
+	then
+		echo "$PACK is not installed "
+		#printf '\u274c\n'
+		$dependency_not_met=true
+	else
+		echo "$PACK is installed "
+		#printf '\u2714\n'
+	fi
 done
 
-# installing tmux
-PACK='tmux'
-echo ":: Checking if $PACK is installed."
-if ! command -v $PACK 1> /dev/null
-then
-    echo ":: $PACK is not be found, installing..."
-    $PACKMANAGER $PACK
-    echo ":: DONE"
-else
-	echo ":: $PACK is installed, proceeding..."
+if [ "$dependency_not_met" = true ]; then
+	echo "One or more dependencies are not met, please intall them first!"
+	exit
 fi
 
-echo ":: Creating soft link... (.tmux)"
-ln -s -f $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -s -f $HOME/.dotfiles/.tmux.conf.local $HOME/.tmux.conf.local
-echo ":: DONE"
-
-
-# installing zsh
-PACK='zsh'
-echo ":: Checking if $PACK is installed."
-if ! command -v $PACK 1> /dev/null
-then
-    echo ":: $PACK is not be found, installing..."
-    $PACKMANAGER $PACK
-    echo ":: DONE"
-else
-	echo ":: $PACK is installed, proceeding..."
-fi
 
 # changing shell to zsh, if current shell is not zsh
 echo ":: Changing shell to zsh..."
@@ -84,25 +76,9 @@ echo ":: Changing shell to zsh..."
 shell=$(echo $SHELL)
 [ ${shell##*/} = "zsh" ] && echo "zsh is already the default shell, skipping" || (chsh -s $(which zsh) && echo ":: DONE")
 
-# installing ohmyzsh
-# checking if curl is installed
-
-# this can later be a for each checking an array that holds
-# all the dependencies
-
-PACK='curl'
-echo ":: Checking if $PACK is installed."
-if ! command -v $PACK 1> /dev/null
-then
-    echo ":: $PACK is not be found, installing..."
-    $PACKMANAGER $PACK
-    echo ":: DONE"
-else
-	echo ":: $PACK is installed, proceeding..."
-fi
-
 # checking if .oh-my-zsh exists
-[ -d ~/.oh-my-zsh ] && echo ".oh-my-zsh folder exists, assuming it's installed correctly" || (echo ":: Installing ohmyzsh" && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo ":: DONE")
+echo ":: Installing .ohmyzsh"
+[ -d "$HOME/.oh-my-zsh" ] && echo ".oh-my-zsh folder exists, assuming it's installed correctly" || (echo ":: Installing ohmyzsh" && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo ":: DONE")
 
 echo ":: Creating soft link... (.zshrc)"
 ln -s -f $HOME/.dotfiles/.zshrc $HOME/.zshrc
@@ -119,14 +95,18 @@ echo ":: DONE"
 echo ":: Installing separate plugins for ohmyzsh"
 
 echo "Installing zsh-autosuggestions"
-[ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] && echo "Installed, skip." || (git clone https://github.com/zsh-users/zsh-autosuggestions ${zsh_custom:-${zsh:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions && echo ":: DONE")
+[ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] && echo "Installed, skip." || (git clone https://github.com/zsh-users/zsh-autosuggestions ${zsh_custom:-${zsh:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions && echo ":: DONE")
 
 echo "Installing zsh-syntax-highlighting"
-[ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] && echo "Installed, skip." || (git clone https://github.com/zsh-users/zsh-syntax-highlighting ${zsh_custom:-${zsh:-~/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting && echo ":: DONE")
+[ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ] && echo "Installed, skip." || (git clone https://github.com/zsh-users/zsh-syntax-highlighting ${zsh_custom:-${zsh:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting && echo ":: DONE")
 
 echo "Installing zsh-completions"
-[ -d ~/.oh-my-zsh/custom/plugins/zsh-completions ] && echo "Installed, skip." || (git clone https://github.com/zsh-users/zsh-completions ${zsh_custom:-${zsh:-~/.oh-my-zsh}/custom}/plugins/zsh-completions && echo ":: DONE")
+[ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ] && echo "Installed, skip." || (git clone https://github.com/zsh-users/zsh-completions ${zsh_custom:-${zsh:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-completions && echo ":: DONE")
 
+echo ":: Creating soft link... (.tmux)"
+ln -s -f $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s -f $HOME/.dotfiles/.tmux.conf.local $HOME/.tmux.conf.local
+echo ":: DONE"
 
 # use parameter to decide if .xinitrc is needed to be installed into system
 # todo
